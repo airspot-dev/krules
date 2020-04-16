af35e21e570addea0814cfdb265eebd1cdb162ff
Alberto Degli Esposti <alberto@airspot.tech>
2020-04-16 17:43:53 +0200

    arg processors working - needs to remove references to with_self

--- krules_core/base_functions/__init__.py
+++ krules_core/base_functions/__init__.py
@@ -13,64 +13,65 @@
 
 from abc import ABCMeta, abstractmethod
 #import jsonpath_rw_ext as jp
+from krules_core.arg_processors import processors
 
 
-class with_payload(object):
-
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, payload):
-        self._result = self.func(payload)
-        return self._result
-
-    def __repr__(self):
-        if not hasattr(self, "_result"):
-            return "[not called]"
-        return str(self._result)
-
-    def result(self):
-        return getattr(self, "_result", None)
-
-# TODO: with_payload_jp
-
-# TODO: need testing
-class with_subject(object):
-
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, subject):
-        self._result = self.func(subject)
-        return self._result
-
-    def __repr__(self):
-        if not hasattr(self, "_result"):
-            return "[not called]"
-        return str(self._result)
-
-    def result(self):
-        return getattr(self, "_result", None)
-
-# TODO: with_subject_jp
-
-# TODO: test
-class with_self(object):
-
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, _class):
-        self._result = self.func(_class)
-        return self._result
-
-    def __repr__(self):
-        if not hasattr(self, "_result"):
-            return "[not called]"
-        return str(self._result)
-
-    def result(self):
-        return getattr(self, "_result", None)
+# class with_payload(object):
+#
+#     def __init__(self, func):
+#         self.func = func
+#
+#     def __call__(self, payload):
+#         self._result = self.func(payload)
+#         return self._result
+#
+#     def __repr__(self):
+#         if not hasattr(self, "_result"):
+#             return "[not called]"
+#         return str(self._result)
+#
+#     def result(self):
+#         return getattr(self, "_result", None)
+#
+# # TODO: with_payload_jp
+#
+# # TODO: need testing
+# class with_subject(object):
+#
+#     def __init__(self, func):
+#         self.func = func
+#
+#     def __call__(self, subject):
+#         self._result = self.func(subject)
+#         return self._result
+#
+#     def __repr__(self):
+#         if not hasattr(self, "_result"):
+#             return "[not called]"
+#         return str(self._result)
+#
+#     def result(self):
+#         return getattr(self, "_result", None)
+#
+# # TODO: with_subject_jp
+#
+# # TODO: test
+# class with_self(object):
+#
+#     def __init__(self, func):
+#         self.func = func
+#
+#     def __call__(self, _class):
+#         self._result = self.func(_class)
+#         return self._result
+#
+#     def __repr__(self):
+#         if not hasattr(self, "_result"):
+#             return "[not called]"
+#         return str(self._result)
+#
+#     def result(self):
+#         return getattr(self, "_result", None)
 
 
 class RuleFunctionBase:
@@ -82,42 +83,33 @@ class RuleFunctionBase:
     payload = {}
     message = ""
 
-    def __init__(self, *args, **kwargs) :
+    def __init__(self, *args, **kwargs):
         self._args = args
         self._kwargs = kwargs
 
-    def _get_args(self, _cinst):
-        for _a in self._args:
-            if isinstance(_a, with_self):
-                yield _a(_cinst)
-            elif isinstance(_a, with_payload):
-                yield _a(_cinst.payload)
-            elif isinstance(_a, with_subject):
-                yield _a(_cinst.subject)
-            elif isinstance(_a, (list, tuple, dict)):
-                yield self._parse_params(_a, _cinst.payload)
-            else:
-                yield _a
+    def _get_args(self, instance):
 
-    def _get_kwargs(self, _cinst):
-        _kwargs = self._kwargs.copy()
-        return self._parse_params(_kwargs, _cinst)
+        args = list(self._args)
+        for i in range(len(self._args)):
+            v = self._args[i]
+            for processor in processors:
+                if processor.interested_in(v):
+                    args[i] = processor.process(instance, v)
+                    break
+                else:
+                    pass
+        return args
 
-    def _parse_params(self, params, _cinst):
-        for k in params:
-            if isinstance(params, dict):
-                index = k
-            else:
-                index = params.index(k)
-            if isinstance(params[index], with_payload):
-                params[index] = params[index](_cinst.payload)
-            if isinstance(params[index], with_subject):
-                params[index] = params[index](_cinst.subject)
-            if isinstance(params[index], with_self):
-                params[index] = params[index](_cinst)
-            #elif isinstance(params[index], (list, tuple, dict)):
-            #    params[index] = self._parse_params(params[index], _cinst.payload)
-        return params
+    def _get_kwargs(self, instance):
+
+        kwargs = self._kwargs.copy()
+        for key in kwargs:
+            for processor in processors:
+                if processor.interested_in(kwargs[key]):
+                    kwargs[key] = processor.process(instance, kwargs[key])
+                    break
+        return kwargs
+
 
     @abstractmethod
     def execute(self, *args, **kwargs):
