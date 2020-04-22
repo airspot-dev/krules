2189ffe0b3583ccaba26c2cc615ec415d3541d4b
Alberto Degli Esposti <alberto@airspot.tech>
2020-04-22 16:55:16 +0200

    removed subject_storage provider

--- mongodb_subjects_storage/test_storage_atomic_ops.py
+++ mongodb_subjects_storage/test_storage_atomic_ops.py
@@ -2,19 +2,16 @@
 import os
 
 from dependency_injector import providers
-from krules_core.providers import subject_storage,subject_storage_factory
+from krules_core.providers import subject_storage_factory
 
 from .storage_impl import SubjectsMongoStorage
 
-subject_storage.override(
-    providers.Factory(SubjectsMongoStorage)
-)
 
 mongodb_url = os.environ.get("TEST_MONGODB_SUBJECTS_STORAGE_URL", "mongodb://localhost:27017/admin")
 database = os.environ.get("TEST_MONGODB_SUBJECTS_STORAGE_DATABASE", "test")
 
 subject_storage_factory.override(
     providers.Factory(
-        lambda x: subject_storage(x, database, "test-subjects-atomic-ops", client_args=(mongodb_url,),
-                                  use_atomic_ops_collection=True))
+        lambda x: SubjectsMongoStorage(x, database, "test-subjects-atomic-ops", client_args=(mongodb_url,),
+                                       use_atomic_ops_collection=True))
 )
