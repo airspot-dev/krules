4d79d7848b8dde5b66884e9fb4a03b7586fc7b41
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-05-15 15:55:27 +0200

    fix version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='krules-env',
-    version="0.0.1",
+    version="0.2.0",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules environment base package",
