f3fc3e44cd5833f8890a244bb41913b54fffeafe
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-06-26 11:30:18 +0200

    remove unusued file

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-
-bumpversion --current-version $(cat VERSION) patch VERSION --allow-dirty
-VERSION=$(cat VERSION)
-
-docker build -t rulesset-image-base:$VERSION -t ${DOCKER_REGISTRY}/rulesset-image-base:$VERSION .
