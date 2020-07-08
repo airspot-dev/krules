972289ca15f2910380453b8fd8e1690fcb1ed95a
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-07-08 15:09:55 +0200

    update krules-dispatcher version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='krules-env',
-    version="0.2.10",
+    version="0.2.11",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules environment base package",
@@ -21,7 +21,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
     ],
     install_requires=[
-        "krules-dispatcher-cloudevents==0.2.9",
+        "krules-dispatcher-cloudevents==0.2.10",
     ],
     setup_requires=[
         'pytest-runner',
