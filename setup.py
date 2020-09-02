2dcc6d897114a6185d4a016b78abb23f32e2273c
LorenzoBtz <lorenzobtz@MBPdiLorenzo5.lan>
2020-09-02 18:15:52 +0200

    update core version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='krules-env',
-    version="0.4.0",
+    version="0.4.1",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules environment base package",
@@ -21,7 +21,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
     ],
     install_requires=[
-        "krules-dispatcher-cloudevents==0.4.0",
+        "krules-dispatcher-cloudevents==0.4.1",
     ],
     setup_requires=[
         'pytest-runner',
