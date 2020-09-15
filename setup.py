d5726d2d219a3ecc2f847017a944cd1c897fb4e1
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-09-15 11:55:20 +0200

    update core version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name='krules-subjects-storage-mongodb',
-    version="0.4.2",
+    version="0.4.3",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules mongodb subjects storage implementation",
@@ -33,7 +33,7 @@ setup(
     ],
     install_requires=[
         'pymongo>=3.10.1',
-        'krules-core==0.4.2',
+        'krules-core==0.4.3',
     ],
     setup_requires=[
         'pytest-runner',
