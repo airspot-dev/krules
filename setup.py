8b3f5b75b5e7c5679938f0c66704c743c182167d
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-09-15 11:32:34 +0200

    update core version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='krules-dispatcher-cloudevents',
-    version="0.4.2",
+    version="0.4.3",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules cloudevents dispatcher",
@@ -23,7 +23,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
     ],
     install_requires=[
-        'krules-core==0.4.2',
+        'krules-core==0.4.3',
     ],
     setup_requires=[
         'pytest-runner',
