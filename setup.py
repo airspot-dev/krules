087f49ee394bc68b345c74e216c839035678144b
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-09-15 11:58:48 +0200

    core version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name='krules-subjects-storage-redis',
-    version="0.4.2",
+    version="0.4.3",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules redis subjects storage implementation",
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         'redis>=3.3.5',
         'anyjson>=0.3.3',
-        'krules-core==0.4.2',
+        'krules-core==0.4.3',
     ],
     setup_requires=[
         'pytest-runner',
