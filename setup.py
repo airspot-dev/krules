c65f06e21ddd73186e7d8ca2a31f625e881f789c
LorenzoBtz <lorenzobtz@MacBook-Pro-di-Lorenzo-5.local>
2020-09-18 11:05:56 +0200

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='krules-core',
-    version="0.4.3-KRUL-153",
+    version="0.4.4",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules Python core package",
