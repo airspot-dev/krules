2975c83f5e9aa7ffdf3bfcc9223b68afdbd88eae
LorenzoBtz <lorenzobtz@MBPdiLorenzo5.lan>
2020-12-03 17:35:14 +0100

    update core dependency

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name='krules-subjects-storage-redis',
-    version="0.6",
+    version="0.7",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules redis subjects storage implementation",
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         'redis>=3.3.5',
         'anyjson>=0.3.3',
-        'krules-core==0.6',
+        'krules-core==0.7',
     ],
     setup_requires=[
         'pytest-runner',
