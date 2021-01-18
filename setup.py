5b35514f3fdd2b5104dcdaa19a5d10f504b4fa52
LorenzoBtz <lorenzo@airspot.tech>
2021-01-18 14:40:57 +0100

    add misc in base_functions import

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='krules-core',
-    version="0.7",
+    version="0.8",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules Python core package",
