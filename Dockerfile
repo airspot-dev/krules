a2b670f7649bcd76bd839d0f773b216812e6aafd
LorenzoBtz <lorenzo@airspot.tech>
2021-02-04 18:19:01 +0100

    refactoring to use new env version

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,7 @@ RUN apk add --no-cache --virtual .build-deps g++ python3-dev && \
     apk add --no-cache --update python3 && \
     pip3 install --upgrade pip setuptools
 
-RUN pip3 install pyyaml anyjson \
-    jsonpath-rw jsonpath-rw-ext python-dateutil pytz requests gunicorn \
-    flask json-logging
+RUN pip3 install anyjson jsonpath-rw jsonpath-rw-ext python-dateutil gunicorn
 
 ADD ./app /app
 
--- app/main.py
+++ app/main.py
@@ -1,80 +1,26 @@
 import json
 import os
-import sys
-import logging
 from datetime import datetime
-import json_logging
-import importlib
 
 from dependency_injector import providers
-from flask import Flask
 from flask import Response
 from flask import request
-from flask import g
+from krules_core.providers import subject_factory
 
 from krules_core.route.router import DispatchPolicyConst
-from krules_core.providers import event_router_factory
-from krules_core.utils import load_rules_from_rulesdata
-
-
-import krules_env
 import env as app_env
 import io
 
 from cloudevents.sdk.event import v1
 from cloudevents.sdk import marshaller
 
-from krules_core.subject.storaged_subject import Subject
+from krules_env.flask import KRulesApp
 
-from krules_core.providers import (
-            subject_factory,
-        )
-
-from flask_g_wrap import g_wrap
-
-app = Flask("ruleset")
-
-json_logging.ENABLE_JSON_LOGGING = True
-json_logging.init_flask()
-json_logging.init_request_instrument(app)
-
-# app.logger.setLevel(int(os.environ.get("LOGGING_LEVEL", logging.INFO)))
-# app.logger.propagate = False
-
-# TODO: KRUL-47
-logger = logging.getLogger(app.name)
-logger.setLevel(int(os.environ.get("LOGGING_LEVEL", logging.INFO)))
-logger.addHandler(logging.StreamHandler(sys.stdout))
-logger.propagate = False
-
-logger_core = logging.getLogger("__core__")
-logger_core.setLevel(int(os.environ.get("CORE_LOGGING_LEVEL", logging.ERROR)))
-logger_core.addHandler(logging.StreamHandler(sys.stdout))
-logger_core.propagate = False
-
-logger_router = logging.getLogger("__router__")
-logger_router.setLevel(int(os.environ.get("ROUTER_LOGGING_LEVEL", logging.ERROR)))
-logger_router.addHandler(logging.StreamHandler(sys.stdout))
-logger_router.propagate = False
-
-req_logger = logging.getLogger("flask-request-logger")
-req_logger.setLevel(logging.ERROR)
-req_logger.propagate = False
-
-krules_env.init()
+app = KRulesApp("ruleset")
 app_env.init()
-subject_factory.override(providers.Factory(lambda *args, **kw: g_wrap(subject_factory.cls, *args, **kw)))
-
-try:
-    m_rules = importlib.import_module("ruleset")
-except ModuleNotFoundError:
-    m_rules = importlib.import_module("rules")
 
 
-load_rules_from_rulesdata(m_rules.rulesdata)
-
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['POST'], auto_store_subjects=True)
 def main():
     start_time = datetime.now()
     try:
@@ -89,8 +35,6 @@ def main():
         app.logger.debug("RCVR: {}".format(event_data))
         event_type = event_info.get("type")
         subject = event_info.get("subject", "sys-0")
-
-        g.subjects = []
         # TODO: important!!
         # need to find a way to avoid a return of messages from the same service
         # (for example when resending it again after intercepted in the first time)
@@ -102,33 +46,30 @@ def main():
 
         event_info["originid"] = event_info.get("originid", event_info.get("id"))
 
-        logger.debug("subject: {}".format(subject))
-        logger.debug("event_data: {}".format(event_data))
-
-        from dependency_injector import providers
+        app.logger.debug("subject: {}".format(subject))
+        app.logger.debug("event_data: {}".format(event_data))
 
         subject = subject_factory(name=subject, event_info=event_info, event_data=event_data)
 
         event_data["_event_info"] = event_info  # TODO: KRUL-155
 
         try:
-            event_router_factory().route(
+            app.router.route(
                 event_type, subject, event_data,
                 dispatch_policy=dispatch_policy
             )
         finally:
-            for sub in g.subjects:
-                sub.store()
+            pass
 
         exec_time = (datetime.now() - start_time).total_seconds()
-        logger.info("Event",
-                    extra={'props': {
-                                'event_info': event_info,
-                                'type': event_type,
-                                'subject': subject.name,
-                                'exec_time': exec_time,
-                                #'headers': list(headers.keys())
-                    }})
+        app.logger.info("Event",
+                        extra={'props': {
+                            'event_info': event_info,
+                            'type': event_type,
+                            'subject': subject.name,
+                            'exec_time': exec_time,
+                            # 'headers': list(headers.keys())
+                        }})
         return Response(status=200)
 
     except Exception as ex:
