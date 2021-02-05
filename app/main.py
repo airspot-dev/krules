b75b794501dfa071e924437f85619cd94be444d0
AdE @airspot <ade8850@gmail.com>
2021-02-05 15:21:40 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -91,12 +91,7 @@ def main():
         subject = event_info.get("subject", "sys-0")
 
         g.subjects = []
-        # TODO: important!!
-        # need to find a way to avoid a return of messages from the same service
-        # (for example when resending it again after intercepted in the first time)
-        # this workaround only works when in a knative service or at least when SOURCE environment
-        # variable is set
-
+        # TODO: stiil deeded ? (block from same source)
         if event_info["source"] == os.environ.get("K_SERVICE", os.environ.get("SOURCE")):
             return Response(status=201)
 
