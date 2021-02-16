a1cb59fdde862b235cc7f0d88aed3683bc9cc910
LorenzoBtz <lorenzo@airspot.tech>
2021-02-16 11:29:28 +0100

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='krules-core',
-    version="0.8.2",
+    version="0.9",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules Python core package",
