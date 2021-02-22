a0ea97e0efc7fa623047e2df5618a7996da75797
LorenzoBtz <lorenzo@airspot.tech>
2021-02-22 16:28:37 +0100

    update core deps

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name='krules-subjects-storage-mongodb',
-    version="0.8.2",
+    version="0.8.3",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules mongodb subjects storage implementation",
@@ -33,7 +33,7 @@ setup(
     ],
     install_requires=[
         'pymongo==3.10.1',
-        'krules-core==0.8.2',
+        'krules-core==0.8.3.1',
     ],
     setup_requires=[
         'pytest-runner',
