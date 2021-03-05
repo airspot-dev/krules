6fae2bf1103e705c8e0f624b9cb683885b80a88a
LorenzoBtz <lorenzo@airspot.tech>
2021-03-05 16:07:27 +0100

    update core deps

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='krules-dispatcher-cloudevents',
-    version="0.8.3.1",
+    version="0.8.5",
     author="Alberto Degli Esposti",
     author_email="alberto@arispot.tech",
     description="KRules cloudevents dispatcher",
@@ -23,7 +23,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
     ],
     install_requires=[
-        'krules-core==0.8.3.1',
+        'krules-core==0.8.5',
         'cloudevents==1.2.0',
         'pytz==2020.5',
         'requests==2.25.1'
