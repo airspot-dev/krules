cff0a46246d2c2a6670b373cd7e5426082026741
ade8850 @airspot <ade8850@gmail.com>
2021-04-26 18:09:07 +0200

    small fixes

--- scripts/ext_modules/base/__init__.py
+++ scripts/ext_modules/base/__init__.py
@@ -18,7 +18,7 @@ deploy__environ = [
 ishell__exec_lines = [
     "import krules_env, env",
     "krules_env.init()",
-    "env.init()",
+    #"env.init()",
     "from krules_core.providers import subject_factory",
     "from krules_core.providers import event_router_factory",
     "router = event_router_factory()",
