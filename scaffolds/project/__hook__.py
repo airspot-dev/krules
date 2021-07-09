2483b25313a1480031bfea9b1c5862a277ce2fba
ade8850 @airspot <ade8850@gmail.com>
2021-07-09 14:04:32 +0200

    scaffolds => templates

--- scaffolds/project/__hook__.py
+++
@@ -1,5 +0,0 @@
-
-short_help = "Start a new project"
-
-def on_checkout(ctx, click, dest):
-    import pdb; pdb.set_trace()
---
+++ templates/project/__hook__.py
@@ -0,0 +1,6 @@
+
+short_help = "Start a new project"
+
+def on_checkout(ctx, click, dest) -> bool:
+    import pdb; pdb.set_trace()
+    return True
