32440f84616ecf10bb5ae9c5c6241f8c543a9446
ade8850 @airspot <ade8850@gmail.com>
2021-07-09 14:46:00 +0200

    md doc test

--- templates/project/__hook__.py
+++ templates/project/__hook__.py
@@ -3,8 +3,8 @@ Start a new project
 
 Accept the following environment variables
 
-- At project level (in env.project file)
-PROJECT_NAME:
+## At project level (in env.project file) ##
+*PROJECT_NAME*: Name of the project
 
 """
 
