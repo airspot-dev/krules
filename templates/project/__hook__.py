9eda66985c88d979657a3e93d694e840979882ee
ade8850 @airspot <ade8850@gmail.com>
2021-07-09 14:27:57 +0200

    new doc string format

--- templates/project/__hook__.py
+++ templates/project/__hook__.py
@@ -1,6 +1,12 @@
+"""
+Start a new project
 
-short_help = "Start a new project"
+... TODO ...
+
+"""
+
 
 def on_create(ctx, click, dest) -> bool:
-    import pdb; pdb.set_trace()
+    import pdb;
+    pdb.set_trace()
     return True
