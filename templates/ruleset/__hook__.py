6b01d341ab8cd4db58ee4fe51d9c7819b42d01a4
ade8850 @airspot <ade8850@gmail.com>
2021-07-12 16:52:43 +0200

    ruleset template

--- templates/ruleset/__hook__.py
+++ templates/ruleset/__hook__.py
@@ -3,4 +3,8 @@
 
 The following environment variables are set in **.env** file
 
+- **APP_NAME**: It'll be the name of the ruleset service. If not set defaults to destination directory name
+- **IMAGE_NAME**: Name of the image that will be built. If not set defaults to ${PROJECT_NAME}-${APP_NAME}
+- **SERVICE_API**: If set to **base** (default) a standard kubernetes deployment and service (ClusterIP) will be created.
+  If set to **knative** a knative service will be created with cluster-local visibility
 """
