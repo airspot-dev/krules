4108af769b54c08208ca60dc6e68ebf51057d8e4
AdE <ade8850@gmail.com>
2021-12-07 08:12:15 +0100

    import event_types in ruleset templates

--- templates/dj-ruleset/ruleset.py
+++ templates/dj-ruleset/ruleset.py
@@ -1,5 +1,7 @@
 from krules_core import RuleConst as Const
 from krules_core.base_functions import *
+from krules_core.event_types import *
+
 
 rulename = Const.RULENAME
 subscribe_to = Const.SUBSCRIBE_TO
--- templates/ruleset/ruleset.py
+++ templates/ruleset/ruleset.py
@@ -1,6 +1,8 @@
 import os
 from krules_core import RuleConst as Const
 from krules_core.base_functions import *
+from krules_core.event_types import *
+
 
 rulename = Const.RULENAME
 subscribe_to = Const.SUBSCRIBE_TO
